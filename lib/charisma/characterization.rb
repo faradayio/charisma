de03ea3b8d6f5307e87dd4ba69bdb065be59c961
Seamus Abshere <seamus@abshere.net>
2011-03-29 11:30:39 -0500

    subclass Hash

--- lib/charisma/characterization.rb
+++ lib/charisma/characterization.rb
@@ -1,19 +1,9 @@
 module Charisma
-  class Characterization
-    attr_reader :characteristics
-    
-    def initialize
-      @characteristics = {}
-    end
-    
-    def [](name)
-      characteristics[name]
-    end
-    
+  class Characterization < Hash    
     include Blockenspiel::DSL
     def has(name, options = {}, &blk)
       name = name.to_sym
-      characteristics[name] = Characteristic.new(name, options, &blk)
+      self[name] = Characteristic.new(name, options, &blk)
     end
   end
 end
