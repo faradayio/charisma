643dcf064462015b532e2149ae9f19c8f26850a1
Andy Rossmeissl <andy@rossmeissl.net>
2011-10-19 00:18:41 -0400

    Ruby 1.9 got a little stricter about this

--- test/test_charisma.rb
+++ test/test_charisma.rb
@@ -148,8 +148,8 @@ class TestCharisma < Test::Unit::TestCase
   def test_023_json
     planet = Planet.create :name => 'Pluto'
     spaceship = Spaceship.new :weight => 100, :window_count => 3, :destination => planet
-    assert_equal {:value => 100, :units => 'hollagrams'}, spaceship.characteristics[:weight].as_json
+    assert_equal({:value => 100, :units => 'hollagrams'}, spaceship.characteristics[:weight].as_json)
     assert_equal 3, spaceship.characteristics[:window_count].as_json
-    assert_equal {:name => 'Pluto'}, spaceship.characteristics[:destination].as_json
+    assert_equal({:name => 'Pluto'}, spaceship.characteristics[:destination].as_json)
   end
 end
